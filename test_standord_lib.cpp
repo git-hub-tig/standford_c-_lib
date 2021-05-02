f668548385925ed34682ccf833583800ccc6a602
wang qunsen <qswang@birentech.com>
2021-05-02 19:40:57 +0800

    git add -A && git commit -m

---
+++ test_standord_lib.cpp
@@ -0,0 +1,30 @@
+/**
+ * WeatherStatisticsMain.cpp by Olivia Stone for Code Clinic: C++
+ **/
+
+#include <iostream>
+#include <string>
+
+#include "WeatherStatistics.hpp"
+
+// wqs using the standord c++ lib
+#include "strlib.h"
+#include <bits/stdc++.h>
+#include <fmt/format.h>
+
+using namespace fmt;
+using namespace std;
+
+int main()
+{
+    //wqs below
+    string data = "a,b,c";
+    Vector<string> components = stringSplit(data, ",");
+    print("components is {}\n ", components);
+    // [from "strlib.h"]:
+    // python "asdfADF".lower()
+    string data = "ABC";
+    cout << toLowerCase(data) << endl; // prints abc
+
+    //wqs above
+}
